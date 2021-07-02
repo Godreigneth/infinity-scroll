f0773020a99539cce9cfc0f323bfe59fc2cbf9fc
Godreigneth <godreignethlevidibia@yahoo.com>
2021-07-02 21:40:38 +0000

    Check to see if scrolling near bottom of page, Load More Photos

--- script.js
+++ script.js
@@ -64,3 +64,14 @@ async function getPhotos() {
     // Catch Error Here
   }
 }
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// On Load
+getPhotos();
